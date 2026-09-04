3a852123e68ffeaa4ce5ab01ce41a35e3eb0c963
evfecomo <98635089+evfecomo@users.noreply.github.com>
2026-09-05 00:56:55 +0200

    Update copiar.js
    correpcion en copiar

--- copiar.js
+++ copiar.js
@@ -6,7 +6,6 @@ function copiarTexto() {
 
     navigator.clipboard.writeText(contenidoACopiar);
 
-    // Esta es la línea que faltaba para borrar la caja automáticamente
     cuadroTextoCopiado.value = "";
 }
 
